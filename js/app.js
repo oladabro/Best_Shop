c0656f600fa8a94d2e07b2200a2b03cf4112dc5f
Ola <oladabro@gmail.com>
2021-02-11 10:32:36 +0100

    add active menu item underline

---
+++ js/app.js
@@ -0,0 +1,36 @@
+const menuLi = document.querySelector('.navigation__menu');
+const burger = document.querySelector('.burger');
+const navLinks = document.querySelectorAll('.navigation__menu__item a');
+
+const navSlide = () => {
+  burger.addEventListener('click', () => {
+    //toggle class
+    menuLi.classList.toggle('nav__active');
+    burger.classList.toggle('active');
+    //animate links
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = '';
+      } else {
+        link.style.animation = `navLinkZoom 0.5s ease forwards ${
+          index / 5 + 0.5
+        }s`;
+      }
+    });
+  });
+};
+
+// add style to active menu item
+
+const addStyleToActiveMenuItem = (event) => {
+  navLinks.forEach((el) => {
+    el.classList.remove('active');
+  });
+  event.target.classList.add('active');
+};
+
+navLinks.forEach((el) => {
+  el.addEventListener('click', addStyleToActiveMenuItem);
+});
+
+window.addEventListener('DOMContentLoaded', navSlide);
